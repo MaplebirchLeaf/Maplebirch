6bffba1b61c8653578cf364bc5b6a1958530bff5
MaplebirchLeaf <changyebufu@gmail.com>
2025-04-02 22:59:20 +0800

    Add files via upload

---
+++ scriptEarly/SFRegist.js
@@ -0,0 +1,7 @@
+simpleFrameworks.addto('iModOptions', 'MaplebirchOptions');
+
+simpleFrameworks.addto('ModDegreesBox', 'MaplebirchSanity');
+
+simpleFrameworks.addto('ModMenuBig', 'MaplebirchSystemButton');
+
+simpleFrameworks.addto('ModStatusBar', 'fearcaption');
